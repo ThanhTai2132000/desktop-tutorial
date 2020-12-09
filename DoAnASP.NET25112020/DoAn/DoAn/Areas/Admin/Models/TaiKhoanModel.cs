aee7eaebfce774751442ddcad711bb107a6d7f78
quochero196 <quochero196@gmail.com>
2020-12-09 23:21:25 +0700

    Them Model TaiKhoan

---
+++ DoAnASP.NET25112020/DoAn/DoAn/Areas/Admin/Models/TaiKhoanModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn.Areas.Admin.Models
+{
+    public class TaiKhoanModel
+    {
+        [Key]
+        public string TaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public int MaTaiKhoan { get; set; }
+        [ForeignKey("MaTaiKhoan")]
+        
+
+    }
+}
