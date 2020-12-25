7bf08efc79343158957559020cddc160a0b2407f
quochero196 <quochero196@gmail.com>
2020-12-26 00:33:59 +0700

    Login FB,chua ajax va luu session

--- DoAnASP.NET25112020/DoAn/DoAn/Controllers/LoginController.cs
+++ DoAnASP.NET25112020/DoAn/DoAn/Controllers/LoginController.cs
@@ -7,15 +7,32 @@ using DoAn.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
+
 namespace DoAn.Controllers
 {
     public class LoginController : Controller
     {
-        
-        
-        public IActionResult Index()
+
+
+        public ActionResult Index()
         {
+            //ViewBag.UserName = Convert.ToString(Session["UserName"]);
             return View();
         }
+
+        //[HttpPost, ValidateJsonAntiForgeryToken]
+        //public JsonResult Login(string name, string email)
+        //{
+        //    Session["UserName"] = email;
+        //    return Json(new { success = "True" });
+        //}
+
+        //[HttpPost, ValidateJsonAntiForgeryToken]
+        //public JsonResult LogOut()
+        //{
+        //    Session.Abandon();
+        //    return Json(new { success = "True" });
+        //}
+
     }
 }
