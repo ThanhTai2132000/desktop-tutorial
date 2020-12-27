328147142b87c9d86d13a16ed8b17e9b8a13d5a9
ThanhTai2132000 <74943218+ThanhTai2132000@users.noreply.github.com>
2020-12-27 20:37:36 +0700

    Sử dụng APO trả về danh sách dòng điện thoại Json và binding len view Admin/DongDienThoai/Index

---
+++ DoAnASP.NET25112020/DoAn/DoAn/Areas/API/PhoneTypeListAPIController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoAn.Areas.Admin.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DoAn.Areas.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PhoneTypeListAPIController : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public PhoneTypeListAPIController(DPContext context)
+        {
+
+            _context = context;
+        }
+        // GET: api/<PhoneTypeListController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet]
+        public string GetPhoneTypeList()
+        {
+            var listDDT = _context.DongDienThoai;
+            var listDDTNew = from p in _context.DongDienThoai
+                             select p;
+
+            return JsonConvert.SerializeObject(listDDTNew);
+        }
+
+        // GET api/<PhoneTypeListAPIController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PhoneTypeListAPIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PhoneTypeListAPIController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PhoneTypeListAPIController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
